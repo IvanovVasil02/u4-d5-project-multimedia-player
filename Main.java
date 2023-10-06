816005bd0b46f29aea9144d458ae90021cdcae23
IvanovVasil02 <ivanov.vasil235@gmail.com>
2023-10-06 16:14:45 +0200

    Added ability for the user to choose whether he wants to play an item, create a new list of items or exit the player

--- Main.java
+++ Main.java
@@ -25,90 +25,106 @@ public class Main {
 //        }
 //    }
 
+    int active = 1;
 
     do {
       Scanner input = new Scanner(System.in);
-      for (int i = 0; i < 5; i++) {
-        System.out.println("Enter the " + (i + 1) + " element->");
-        System.out.println("Enter 1 to insert image");
-        System.out.println("Enter 2 to insert audio");
-        System.out.println("Enter 3 to insert video");
-        Scanner scanner = new Scanner(System.in);
-        int choice = scanner.nextInt();
+      switch (active) {
+        case 0:
+          break;
 
-        switch (choice) {
-          case 1: {
-            System.out.println("Enter image title");
-            scanner.nextLine();
-            String title = scanner.nextLine();
-
-            System.out.println("Enter brightness image level");
-            int brightness = scanner.nextInt();
-
-            elements[i] = new Img(title, brightness);
-            break;
-          }
-          case 2: {
-            System.out.println("Enter title");
-            scanner.nextLine();
-            String title = scanner.nextLine();
-
-            System.out.println("Enter audio volume level");
-            int volume = scanner.nextInt();
-
-            System.out.println("Enter audio duration");
-            int duration = scanner.nextInt();
-
-            elements[i] = new Audio(title, volume, duration);
-            break;
-          }
-          case 3: {
-            System.out.println("Enter title");
-            scanner.nextLine();
-            String title = scanner.nextLine();
-
-            System.out.println("Enter video volume level");
-            int volume = scanner.nextInt();
-
-            System.out.println("Enter brightness image level");
-            int brightness = scanner.nextInt();
-
-            System.out.println("Enter video duration");
-            int duration = scanner.nextInt();
-
-            elements[i] = new Video(title, volume, brightness, duration);
-            break;
-          }
-        }
-      }
-
-      System.out.println("Choose an element to reproduce from 1 to 5");
-      int choice = input.nextInt();
-
-      switch (choice) {
         case 1:
-          Multimedia.checkInstance(elements[0]);
-          break;
-        case 2:
-          Multimedia.checkInstance(elements[1]);
-          break;
-        case 3:
-          Multimedia.checkInstance(elements[2]);
-          break;
-        case 4:
-          Multimedia.checkInstance(elements[3]);
-          break;
-        case 5:
-          Multimedia.checkInstance(elements[4]);
-          break;
-      }
+          for (int i = 0; i < 5; i++) {
+            System.out.println("Enter the " + (i + 1) + " element or enter 0 to exit");
+            System.out.println("Enter 1 to insert image");
+            System.out.println("Enter 2 to insert audio");
+            System.out.println("Enter 3 to insert video");
+            Scanner scanner = new Scanner(System.in);
+            int choice = scanner.nextInt();
 
-      break;
+            switch (choice) {
+              case 1: {
+                System.out.println("Enter image title");
+                scanner.nextLine();
+                String title = scanner.nextLine();
+
+                System.out.println("Enter brightness image level");
+                int brightness = scanner.nextInt();
+
+                elements[i] = new Img(title, brightness);
+                break;
+              }
+              case 2: {
+                System.out.println("Enter title");
+                scanner.nextLine();
+                String title = scanner.nextLine();
+
+                System.out.println("Enter audio volume level");
+                int volume = scanner.nextInt();
+
+                System.out.println("Enter audio duration");
+                int duration = scanner.nextInt();
+
+                elements[i] = new Audio(title, volume, duration);
+                break;
+              }
+              case 3: {
+                System.out.println("Enter title");
+                scanner.nextLine();
+                String title = scanner.nextLine();
+
+                System.out.println("Enter video volume level");
+                int volume = scanner.nextInt();
+
+                System.out.println("Enter brightness image level");
+                int brightness = scanner.nextInt();
+
+                System.out.println("Enter video duration");
+                int duration = scanner.nextInt();
+
+                elements[i] = new Video(title, volume, brightness, duration);
+                break;
+              }
+            }
+          }
+
+          System.out.println("press 2 to play an item, 1 to create New list, 0 to exit");
+          active = input.nextInt();
+
+        case 2:
+          while (true) {
+            System.out.println("Choose an element to reproduce from 1 to 5");
+            input.nextLine();
+            int choice1 = input.nextInt();
+
+            switch (choice1) {
+              case 1:
+                Multimedia.checkInstance(elements[0]);
+                break;
+              case 2:
+                Multimedia.checkInstance(elements[1]);
+                break;
+              case 3:
+                Multimedia.checkInstance(elements[2]);
+                break;
+              case 4:
+                Multimedia.checkInstance(elements[3]);
+                break;
+              case 5:
+                Multimedia.checkInstance(elements[4]);
+                break;
+            }
+
+            System.out.println("Enter 2 to continue, 1 to create New list, 0 to exit");
+            input.nextLine();
+            active = input.nextInt();
+            if (active != 2) {
+              break;
+            }
+          }
+      }
 
     } while (true);
-
-
   }
-
 
 }
